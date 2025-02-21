3dfbddc1235a8fac7d331fe2e262a871f1bca265
Máté Brösztl <brosztl.mate@gmail.com>
2025-02-21 20:44:26 +0100

    Add system tray icon using Hardcodet.NotifyIcon.Wpf
    Introduced a system tray icon for SecureMe using the Hardcodet.NotifyIcon.Wpf library.
    
    In App.xaml:
    - Added XML namespace for Hardcodet.NotifyIcon.Wpf.
    - Added TaskbarIcon resource with properties and context menu.
    
    In App.xaml.cs:
    - Imported Hardcodet.Wpf.TaskbarNotification namespace.
    - Added private field _trayIcon for TaskbarIcon instance.
    - Overrode OnStartup to initialize _trayIcon.
    - Overrode OnExit to dispose of _trayIcon and handle logout.
    - Added Exit_Click event handler for exit menu item.
    
    In SecureMe.csproj:
    - Added reference to Hardcodet.NotifyIcon.Wpf library.

--- SecureMe/App.xaml.cs
+++ SecureMe/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Hardcodet.Wpf.TaskbarNotification;
 using System.Windows;
 
 namespace SecureMe
@@ -14,6 +15,12 @@ namespace SecureMe
     /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon _trayIcon;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _trayIcon = (TaskbarIcon)FindResource("TrayIcon");
+        }
         protected override void OnExit(ExitEventArgs e)
         {
             try
@@ -38,5 +45,24 @@ namespace SecureMe
             base.OnExit(e);
         }
 
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                User user = UserManager.LoadUser();
+                if (user != null)
+                {
+                    user.IsLoginWithMasterPassword = false;
+                    UserManager.SaveUser(user);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error on exit: {ex.Message}");
+            }
+
+            _trayIcon?.Dispose();
+            Application.Current.Shutdown();
+        }
     }
 }
